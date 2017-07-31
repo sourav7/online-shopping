99926c9f5d5b35de09141f9925c42ec937f81a66
Sourav_Debnath <souravdebnath7@gmail.com>
2017-07-30 17:19:58 -1200

    Updated 30/07/2017

---
+++ onlineshopping/src/main/java/org/codemaster/controller/PageController.java
@@ -0,0 +1,15 @@
+package org.codemaster.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Spring Web mvc.");
+		return mv;
+	}
+}
