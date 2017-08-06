b67f70b8b8fa13ab331b0d3bdb84ad9ad073315a
Sourav_Debnath <souravdebnath7@gmail.com>
2017-08-05 19:49:49 -1200

    JavaScript file refactored

--- onlineshopping/src/main/webapp/resources/js/myapp.js
+++ onlineshopping/src/main/webapp/resources/js/myapp.js
@@ -11,7 +11,7 @@ $(function() {
 		$('#listProducts').addClass('active');
 		break;
 	default:
-		if(menu == "Home") break;
+		if(menu === "Home") break;
 		$('#listProducts').addClass('active');
 		$('#a_' + menu).addClass('active');
 		break;
